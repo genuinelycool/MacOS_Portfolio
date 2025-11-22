cda33190ca45a55196424dd67ef07ead19915acb
genuinelycool <genuinelycool@gmail.com>
2025-11-22 19:05:33 +0545

    Configure tailwind and ready to use

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my MacFolio</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to my MacFolio</h1>
     </div>
   );
 };
