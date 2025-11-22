210bb9b2bb90ad222949ff87f4edf33b5dacd4e7
genuinelycool <genuinelycool@gmail.com>
2025-11-23 00:19:16 +0545

    fix spelling mistake for tablet

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -86,7 +86,7 @@ const Welcome = () => {
       </h1>
 
       <div className="small-screen">
-        <p>This Portfolio is designed for desktop/tabled screens only.</p>
+        <p>This Portfolio is designed for desktop/tablet screens only.</p>
       </div>
     </section>
   );
